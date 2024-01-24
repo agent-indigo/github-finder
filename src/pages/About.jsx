85089b295ae8788ac17be1f8f74a124668f138eb
agent-indigo <bradenhynes@gmail.com>
2024-01-24 17:09:12 -0330

    Completed Chapter 8

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the
+        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          {' '}
+          React Front To Back
+        </a>{' '}
+        course on O'Reilly by
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg'>
+        Version <span>1.0.0</span>
+      </p>
+      <p className='text-lg'>Layout By: <a className="text-blue-800" href='https://twitter.com/hassibmoddasser'>Hassib Moddasser</a></p>
+      <p className="text-lg">Modified by: <a className="text-blue-800" href="https://github.com/agent-indigo">Braden Hynes (agent-indigo)</a></p>
+    </>
+  )
+}
+export default About
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+function NotFound() {
+  return (
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+            <div className="text-8xl font-bold mb-8">Error 404</div>
+            <p className="text-5xl mb-8">Page not found</p>
+            <Link to='/' className="btn btn-primary btn-lg">
+                <FaHome className="mr-2"/>
+                <p>Back to Home</p>
+            </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default NotFound
