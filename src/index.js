d0b5315ac5c68e87325bd7864f077f51f58b97bb
agent-indigo <bradenhynes@gmail.com>
2024-02-08 12:59:01 -0330

    Optional edit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-)
+const index = (() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'))
+  root.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  )
+})()
+export default index
