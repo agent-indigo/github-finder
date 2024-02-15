4c57eb9db170144c04798e49f556554f4609b831
agent-indigo <bradenhynes@gmail.com>
2024-02-15 01:28:33 -0330

    Optional cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.jsx"],
-  theme: {
-    extend: {},
-  },
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
